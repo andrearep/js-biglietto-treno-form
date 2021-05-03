9172576a095984d2767641c6d358b12dd82005c0
andrearep <andrea.repe@gmail.com>
2021-05-03 17:29:47 +0200

    terminato esercizio

--- js/main.js
+++ js/main.js
@@ -9,9 +9,23 @@ function kmVerifica() {
     return km;
 }
 
+function randomNum() {
+    return Math.floor(Math.random() * 9)
+}
+
+function randomCod(n) {
+    var array = []
+    for (var i = 0; i < n; i++) {
+        array[i] = randomNum();
+    }
+    return array.join("");
+}
+
 function generateTicketPrice() {
     var name = document.getElementById("name").value;
     var age = document.getElementById("age").value;
+    var carrozza = randomNum() + 1;
+    var codCp = randomCod(5);
 
     if (!kmVerifica()) {
         return alert("inserire dei km da percorrere validi")
@@ -19,10 +33,10 @@ function generateTicketPrice() {
         km = kmVerifica()
     }
     console.log(age, name, km);
-    if (age == "19 - 64") {
-
+    if (age == "intero") {
+        document.getElementById("offerta").innerHTML = "nessuno";
     } else {
-        //document.getElementById("offerta").innerHTML = "Sconto" + age;
+        document.getElementById("offerta").innerHTML = age;
     }
     var price = km * 0.21;
 
@@ -34,10 +48,26 @@ function generateTicketPrice() {
     }
     //stampa prezzo
     console.log(price);
-    document.getElementById("price").innerHTML = price;
+    document.getElementById("pass_name").innerHTML = name;
+    document.getElementById("carrozza").innerHTML = carrozza;
+    document.getElementById("code_cp").innerHTML = codCp;
+    document.getElementById("price").innerHTML = price.toFixed(2) + " €";
+
+}
+
+function annulla() {
+    document.getElementById("km").value = ""
+    document.getElementById("name").value = "";
+    document.getElementById("age").value = "";
+    document.getElementById("offerta").innerHTML = "";
+    document.getElementById("pass_name").innerHTML = "";
+    document.getElementById("carrozza").innerHTML = "";
+    document.getElementById("code_cp").innerHTML = ""
+    document.getElementById("price").innerHTML = ""
 }
 
 document.getElementById("generate").addEventListener("click", generateTicketPrice)
+document.getElementById("annulla").addEventListener("click", annulla)
 
 
 
